333b5ea63ce9b0019e20b288ef4771918bdd22f9
frank.shaof <shaofeng41@126.com>
2014-08-16 23:23:45 +0800

    t

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,6 +16,7 @@ if(!system.win&&!system.mac&&!system.xll){//转向手机登陆页面
 
 $(function(){
 	var sserver=new SonicServer({alphabet:'0123456789'});
+	var socket=io.connect(document.domain);
 	$("#find_phone").click(function(){
 		if($(this).hasClass("disable")){
 			return false;
@@ -34,6 +35,6 @@ $(function(){
 		$("#find_phone").click();
 	});
 	$("#token_yes").click(function(){
-		
+		socket.emit("login",{token:$("#token").text()});
 	});
 });
--- startws.js
+++ startws.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 var debug = require('debug')('papafight');
-var app = require('../app');
+var app = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
 });
+
+//websocket server
+global.socket = require('socket.io').listen(app);
