a2d1e87a4ee4823f7a07089ca4db81e6dddddec9
frank.shaof <shaofeng41@126.com>
2014-08-23 22:15:03 +0800

    playerselect

--- public/javascripts/mobile/playerselect.js
+++ public/javascripts/mobile/playerselect.js
@@ -2,6 +2,10 @@ $(function(){
 	var lastTime=null;
 	var storage=new Storage();
 	var token=storage.getItem("token");
+	//没登陆，重定向
+	if(token==null){
+		document.location.href="/mobile";
+	}
 	//建立命名空间为playerselect的socket连接
 	var socket=io.connect(document.domain+":8080/playerselect");
 	//设备加速度
--- public/javascripts/playerselect.js
+++ public/javascripts/playerselect.js
@@ -1,12 +1,30 @@
 $(function(){
+	//动画效果
+	$('#gla_box>ul').roundabout({
+		minOpacity:1,
+		btnNext: ".next",
+		duration: 1000,
+		reflect: true,
+		btnPrev: '.prev',
+		tilt:0,
+		shape: 'figure8'
+	});
 	var storage=new Storage();
 	var token=storage.getItem("token");
+	//没登陆，重定向
+	if(token==null){
+		document.location.href="/";
+	}
 	//建立命名空间为playerselect的websocket连接
 	var socket=io.connect(document.domain+":8080/playerselect");
 	socket.on("playerselect success",function(data){
-		console.log(data);
 		if(data.token==token){
-			$("#content").html(token+"<br/>"+data.orientation);
+			if(data.orientation==ORIENTATION.RIGHT){
+				$("#gla_box .next").click();
+			}
+			else if(data.orientation==ORIENTATION.LEFT){
+				$("#gla_box .prev").click();
+			}
 		}
 	});
 });
