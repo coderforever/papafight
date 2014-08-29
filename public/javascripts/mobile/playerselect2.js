db43383a9bc65dbc395bda528bfbe31c7ea51e9d
frank.shaof <shaofeng41@126.com>
2014-08-29 15:05:13 +0800

    orientation

---
+++ public/javascripts/mobile/playerselect2.js
@@ -0,0 +1,70 @@
+Utils.checkToken();
+
+$(function(){
+	var lastGamma=0;
+	var lastBeta=0;
+	var canStart=false;
+	var lastZ_1=0;
+	var lastZ_2=0;
+	var isAsc=true;
+	//获取token
+	var storage=new Storage();
+	var token=storage.getItem("token");
+	//建立命名空间为playerselect的socket连接
+	var socket=io.connect(document.domain+":8080/playerselect");
+	
+	//设备加速度
+	window.addEventListener("devicemotion",function(evt){
+		if(!canStart){
+			return;
+		}
+		var y=evt.acceleration.y;
+		var z=evt.acceleration.z;
+		$("#z").text(z);
+		lastZ_1=lastZ_2;
+		lastZ_2=z;
+		var compare1=lastZ_1>=0;
+		var compare2=lastZ_2>=0;
+		if(compare1!=compare2){
+			return;
+		}
+		if(!isAsc){
+			if(Math.abs(z)>Math.abs(lastZ_1)){
+				isAsc=true;
+			}
+			return;
+		}
+		//手机左右摆动并且手机屏幕垂直地面(gamma=90)
+		if(Math.abs(lastZ_1)>8 && (lastGamma>60 && lastGamma<=90)){
+			if(Math.abs(z)<Math.abs(lastZ_1)){
+				var orientation=ORIENTATION.RIGHT;
+				if(z<0){
+					orientation=ORIENTATION.LEFT;
+				}
+				isAsc=false;				
+				//发送加速度
+				socket.emit("post playerselect",{token:token,orientation:orientation});
+			}
+			
+		}
+		//出拳选择，屏幕向上（beta=0，gamma=0），y加速度
+		else if((Math.abs(y)>8 && (lastBeta>=0 && lastBeta<=30)) && (lastGamma>=0 && lastGamma<=30)){
+			socket.emit("post playerselected",{token:token});
+			document.location.href="/waitplayer/mobile";				
+		}
+	},true);
+	//屏幕方向
+	window.addEventListener("deviceorientation",function(evt){
+		if(!canStart){
+			return;
+		}
+		lastGamma=Math.abs(evt.gamma);
+		lastBeta=Math.abs(evt.beta);
+ 	},true);
+ 	//等待预加载完成，才能选择玩家
+ 	socket.on("preload success",function(data){
+ 		if(data["token"]==token){
+ 			canStart=true;
+ 		}
+ 	});
+});
--- service/waitplayer.js
+++ service/waitplayer.js
@@ -7,7 +7,6 @@ module.exports = function() {
 
 	var wait_socket=global.socket.of('/waitplayer').on('connection', function(socket) { 
    		socket.on('wait player', function (data) {
-   			console.log(data+"------------------------");
     		var token=data["token"];
     		var role=data["role"];
     		if(players.length==0){
@@ -15,7 +14,6 @@ module.exports = function() {
     		}
     		else{
     			var opponent=players.pop();
-    			console.log(opponent);
     			var anotherToken=opponent.token;
     			var anotherRole=opponent.role;
     			wait_socket.emit('wait success',{token1:token,token2:anotherToken,role1:role,role2:anotherRole});
