cc392857d2ab52b0fcf872774126f1f3499f0351
frank.shaof <shaofeng41@126.com>
2014-09-17 00:21:40 +0800

    ui

--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -17,8 +17,16 @@ RESOURCE.IMAGES=[
 	"/images/playerselect-bg.jpg"
 ];
 RESOURCE.AUDIOS=[
+	"/audios/bomb.wav",
+	"/audios/fight_begin.wav",
+	"/audios/fight_bk.wav",
+	"/audios/loser.wav",
 	"/audios/orientation.wav",
 	"/audios/playerselect.wav",
 	"/audios/selectsuccess.wav",
+	"/audios/punch_no_defend.wav",
+	"/audios/punch_with_defend.wav",
+	"/audios/punch_with_hide.wav",
+	"/audios/winner.wav",
 	"/audios/loading.wav"
 ];
