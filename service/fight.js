d392d8755ef30fd6aa1159ee6cea5fb5c4b824d1
hc <fanhc019@foxmail.com>
2014-09-19 00:01:02 +0800

    hc

---
+++ service/fight.js
@@ -0,0 +1,17 @@
+/*
+	判断手势相关的websocket服务
+*/
+
+module.exports = function() {    
+	var playerselect_socket=global.socket.of('/fight').on('connection', function(socket) { 
+   		socket.on('post preload', function(data){
+   			playerselect_socket.emit('preload success',data);
+   		});
+   		socket.on('post playerselect', function (data) {
+    		playerselect_socket.emit('playerselect success',data);
+  		});
+  		socket.on('post playerselected', function(data){
+  			playerselect_socket.emit('playerselected success',data);
+  		});
+   	});
+}();
