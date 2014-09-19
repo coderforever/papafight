b6ca5f728ab7b761f797475a921cd740346171d0
frank.shaof <shaofeng41@126.com>
2014-09-19 10:13:45 +0800

    ui

--- public/javascripts/fight.js
+++ public/javascripts/fight.js
@@ -6,8 +6,10 @@ $(function(){
 	var my_token=storage.getItem("token");
 	var my_role=storage.getItem("role");
 	//倒计时,告诉手机端，电脑端已经进入fight页面
-	var socket=io.connect(document.domain+":8080/waitplayer");
-	socket.emit("enter fight page",{token:my_token});
+	setTimeout(function(){
+		var socket=io.connect(document.domain+":8080/waitplayer");
+		socket.emit("enter fight page",{token:my_token});
+	},3000);
 	
 	//链接fight的socket
 	// var fight_socket=io.connect(document.domain+":8080/fight");
--- public/javascripts/mobile/fight.js
+++ public/javascripts/mobile/fight.js
@@ -26,4 +26,59 @@ $(function(){
 		}
 	});
 	// var fight_socket=io.connect(document.domain+":8080/fight");
+	//设备加速度
+	window.addEventListener("devicemotion",function(evt){
+		if(!canStart){
+			return;
+		}
+		var y=evt.acceleration.y;
+		var z=evt.acceleration.z;
+		$("#z").text(z);
+		lastZ_1=lastZ_2;
+		lastZ_2=z;
+		var compare1=lastZ_1>=0;
+		var compare2=lastZ_2>=0;
+		if(compare1!=compare2){
+			isAsc=true;
+			return;
+		}
+		if(!isAsc){
+			return;
+		}
+		//手机左右摆动并且手机屏幕垂直地面(gamma=90)
+		if(Math.abs(lastZ_1)>6 && (lastGamma>60 && lastGamma<=120)){
+
+			if(Math.abs(z)<Math.abs(lastZ_1)){
+				var orientation=ORIENTATION.RIGHT;
+				if(lastZ_1<0){
+					orientation=ORIENTATION.LEFT;
+				}
+				isAsc=false;				
+				//发送加速度
+				// socket.emit("post playerselect",{token:token,orientation:orientation});
+				$("#console").append("escape");
+				canStart=false;
+				setTimeout(function(){
+					canStart=true;
+					lastZ_1=0;
+					lastZ_2=0;
+				},500);
+			}
+			
+		}
+		//出拳选择，屏幕向上（beta=0，gamma=0），y加速度；防守时候看beta趋近于90
+		else if((Math.abs(y)>8 && (lastBeta>=0 && lastBeta<=30)) && (lastGamma>=0 && lastGamma<=30)){
+			// socket.emit("post playerselected",{token:token});
+			$("#console").append("attack");			
+		}
+	},true);
+	//屏幕方向
+	window.addEventListener("deviceorientation",function(evt){
+		if(!canStart){
+			return;
+		}
+		//beta--绕x轴，gamma--绕y轴，alpha--绕z轴
+		lastGamma=Math.abs(evt.gamma);
+		lastBeta=Math.abs(evt.beta);
+ 	},true);
 });
