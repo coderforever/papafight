bd0b61a1d70221655f2fc6d285c15bb5fe5b5c42
frank.shaof <shaofeng41@126.com>
2014-09-19 13:31:53 +0800

    yu

--- public/javascripts/fight.js
+++ public/javascripts/fight.js
@@ -12,5 +12,29 @@ $(function(){
 	},3000);
 	
 	//链接fight的socket
-	// var fight_socket=io.connect(document.domain+":8080/fight");
+	var fight_socket=io.connect(document.domain+":8080/fight");
+	fight_socket.on("attack",function(data){
+		console.log("attack");
+		console.log(data);
+	});
+	fight_socket.on("attack result",function(data){
+		console.log("attack result");
+		console.log(data);
+	});
+	fight_socket.on("defend",function(data){
+		console.log("defend");
+		console.log(data);
+	});
+	fight_socket.on("nodefend",function(data){
+		console.log("nodefend");
+		// console.log(data);
+	});
+	fight_socket.on("escapeLeft",function(data){
+		console.log("escapeLeft");
+		console.log(data);
+	});
+	fight_socket.on("escapeRight",function(data){
+		console.log("escapeRight");
+		console.log(data);
+	});
 });
